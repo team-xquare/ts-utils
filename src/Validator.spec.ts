b5882196abfe885ee24ffa6bcb6a749ace3a8565
eungyeole <eungyeole@naver.com>
2021-05-24 20:02:59 +0900

    📑 :: [Validator] testCase 수정

--- src/Validator.spec.ts
+++ src/Validator.spec.ts
@@ -2,17 +2,17 @@ import { Validator } from './Validator';
 describe('Validator', ()=>{
     describe('isEmailValid', ()=>{
         it('return `true` 케이스', () => {
-            expect(Validator.isEmailValid('hyeonsu@xquare.com')).toEqual(true);
+            expect(Validator.isEmailValid('eungyeole@xquare.com')).toEqual(true);
         });
         it('should return `false` 케이스', () => {
             expect(Validator.isEmailValid('')).toEqual(false);
-            expect(Validator.isEmailValid('hyeonsu')).toEqual(false);
+            expect(Validator.isEmailValid('eungyeole')).toEqual(false);
             expect(Validator.isEmailValid('1234')).toEqual(false);
-            expect(Validator.isEmailValid('hyeonsu@')).toEqual(false);
-            expect(Validator.isEmailValid('hyeonsu@xquare')).toEqual(false);
-            expect(Validator.isEmailValid('hyeonsu@xquare.')).toEqual(false);
-            expect(Validator.isEmailValid('hyeonsu@xquare.123')).toEqual(false);
-            expect(Validator.isEmailValid('hyeonsu@xquare.com123')).toEqual(false);
+            expect(Validator.isEmailValid('eungyeole@')).toEqual(false);
+            expect(Validator.isEmailValid('eungyeole@xquare')).toEqual(false);
+            expect(Validator.isEmailValid('eungyeole@xquare.')).toEqual(false);
+            expect(Validator.isEmailValid('eungyeole@xquare.123')).toEqual(false);
+            expect(Validator.isEmailValid('eungyeole@xquare.com123')).toEqual(false);
         });
     });
     describe('isGradeValid', ()=>{
