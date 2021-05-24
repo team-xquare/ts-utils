47895a256cdbb10bf3ce104224a3637e1b60d8bc
eungyeole <eungyeole@naver.com>
2021-05-24 18:36:06 +0900

    📝 :: [Validator] TestCode 추가

---
+++ src/Validator.spec.ts
@@ -0,0 +1,27 @@
+import { Validator } from './Validator';
+describe('Validator', ()=>{
+    describe('isEmailValid', ()=>{
+        it('return `true` 케이스', () => {
+            expect(Validator.isEmailValid('hyeonsu@xquare.com')).toEqual(true);
+        });
+        it('should return `false` 케이스', () => {
+            expect(Validator.isEmailValid('')).toEqual(false);
+            expect(Validator.isEmailValid('hyeonsu')).toEqual(false);
+            expect(Validator.isEmailValid('1234')).toEqual(false);
+            expect(Validator.isEmailValid('hyeonsu@')).toEqual(false);
+            expect(Validator.isEmailValid('hyeonsu@xquare')).toEqual(false);
+            expect(Validator.isEmailValid('hyeonsu@xquare.')).toEqual(false);
+            expect(Validator.isEmailValid('hyeonsu@xquare.123')).toEqual(false);
+            expect(Validator.isEmailValid('hyeonsu@xquare.com123')).toEqual(false);
+        });
+    });
+    describe('isGradeValid', ()=>{
+        it('return `true` 케이스', () => {
+            expect(Validator.isGradeValid('2411', 2)).toEqual(true);
+        })
+        it('teturn `false` 케이스', () => {
+            expect(Validator.isGradeValid('2411',1)).toEqual(false);
+            expect(Validator.isGradeValid('2411',3)).toEqual(false);
+        })
+    });
+})
