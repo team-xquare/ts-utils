47b436edd2be7d7be63c72689c936f857f85a008
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-05-26 14:13:25 +0900

    ♻️ :: delay 함수 주석 추가

--- dist/delay.d.ts
+++ dist/delay.d.ts
@@ -1,5 +1,1 @@
-/**
- * Wait as long as @param.
- * @param ms Milli Second
- */
 export declare function delay(ms: number): Promise<void>;
--- src/delay.ts
+++ src/delay.ts
@@ -1,3 +1,7 @@
+/**
+ * Wait as long as @param.
+ * @param ms Milli Second
+ */
 export function delay(ms: number): Promise<void> {
   return new Promise(resolve => {
     setTimeout(resolve, ms);
