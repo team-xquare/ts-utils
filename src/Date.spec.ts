4386e3286833642c2607f79e398e3786a6f64fbc
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-06-02 13:28:13 +0900

    🧪 :: [Date] getDateDifference 함수 테스트코드 추가

---
+++ src/Date.spec.ts
@@ -0,0 +1,36 @@
+import { getDateDifference } from './Date';
+
+describe('getDateDifference', () => {
+  it('3초 뒤', async () => {
+    const startDate: Date = new Date();
+    const endDate: Date = new Date();
+    startDate.setSeconds(1);
+    endDate.setSeconds(4);
+    const difference = getDateDifference(endDate, startDate);
+    expect(difference.seconds).toEqual(3);
+  });
+  it('3분 뒤', async () => {
+    const startDate: Date = new Date();
+    const endDate: Date = new Date();
+    startDate.setMinutes(1);
+    endDate.setMinutes(4);
+    const difference = getDateDifference(endDate, startDate);
+    expect(difference.minutes).toEqual(3);
+  });
+  it('3시간 뒤', async () => {
+    const startDate: Date = new Date();
+    const endDate: Date = new Date();
+    startDate.setHours(1);
+    endDate.setHours(4);
+    const difference = getDateDifference(endDate, startDate);
+    expect(difference.hours).toEqual(3);
+  });
+  it('3일 뒤', async () => {
+    const startDate: Date = new Date();
+    const endDate: Date = new Date();
+    startDate.setDate(1);
+    endDate.setDate(4);
+    const difference = getDateDifference(endDate, startDate);
+    expect(difference.days).toEqual(3);
+  });
+});
